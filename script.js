e4e4ed0be64c6dce905d48b16460b6cf0a78c624
alexmarian0210 <alex_marian@estudante.sesisenai.org.b>
2025-09-11 19:34:37 -0300

    tipos de dados java

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
+/*
+console.log("Olá Mundo!");
+
 let nome = "Alex";
 let idade = 21;
 let cidade = "Imbuia - SC";
 let altura = 1.80;
 let ehHumano = true;
 
+
+/*
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof cidade);
+console.log(typeof altura);
+console.log(typeof ehHumano);
+
+
+console.log("nome", nome);
+console.log("idade", idade);
+console.log("cidade", cidade);
+console.log("altura", altura);
+console.log("ehHumano", ehHumano);
+
 console.log("Meu nome é " + nome + ", tenho " + idade + " anos, moro em " + cidade + ", tenho " + altura + "m de altura e é " + ehHumano + " que eu sou humano.");
+
+*/
+
+let texto = "ola mundo";
+let numero = 123;
+let pi = 3.14;
+let booleano = true;
+let nulo = null;
+let indefinido;
+
+console.log(typeof texto);
+console.log(typeof numero);
+console.log(typeof pi);
+console.log(typeof booleano);
+console.log(typeof nulo);
+console.log(typeof indefinido);
+
